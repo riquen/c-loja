a1f14f667304d8211f9a664e3369a96bef3b9db4
Henrique Nunes <henri.arantes1@gmail.com>
2024-03-26 20:51:06 -0300

    some for collections

--- src/c_loja/core.clj
+++ src/c_loja/core.clj
@@ -9,18 +9,6 @@
        (sort-by :preco-total)
        reverse))
 
-(defn top-2
-  [resumo]
-  (take 2 resumo))
-
 (let [pedidos (l.db/todos-os-pedidos)
       resumo (resumo-por-usuario-ordenado pedidos)]
-  (println resumo)
-  (println (first resumo))
-  (println (second resumo))
-  (println (rest resumo))
-  (println (count resumo))
-  (println (class resumo))
-  (println (nth resumo 1))
-  (println (take 2 resumo))
-  (println (top-2 resumo)))
+  (println (some #(> (:preco-total %) 500) resumo)))
