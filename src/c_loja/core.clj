5ac822229261fa16e4d31786f4cff8aa043483cc
Henrique Nunes <henri.arantes1@gmail.com>
2024-03-26 17:03:30 -0300

    working with collections

--- src/c_loja/core.clj
+++ src/c_loja/core.clj
@@ -1,3 +1,30 @@
-(ns c-loja.core)
+(ns c-loja.core
+  (:require [c-loja.db :as l.db]))
 
-(map println ["estoque" "caixa" "produto" "venda" "compra"])
+(defn total-do-item
+  [[_ detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco-unitario 0)))
+
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+       (map total-do-item)
+       (reduce +)))
+
+(defn total-dos-pedidos
+  [pedidos]
+  (->> pedidos
+       (map :itens)
+       (map total-do-pedido)
+       (reduce +)))
+
+(defn quantidade-de-pedidos-e-gasto-total-por-usuario
+  [[usuario, pedidos]]
+  {:usuario-id       usuario
+   :total-de-pedidos (count pedidos)
+   :preco-total      (total-dos-pedidos pedidos)})
+
+(->> (l.db/todos-os-pedidos)
+     (group-by :usuario)
+     (map quantidade-de-pedidos-e-gasto-total-por-usuario)
+     println)
