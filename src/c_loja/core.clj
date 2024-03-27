63a3c8576a4b451ae0a03064575d5dd95a85dde8
Henrique Nunes <henri.arantes1@gmail.com>
2024-03-27 13:32:55 -0300

    lazy and eager

--- src/c_loja/core.clj
+++ src/c_loja/core.clj
@@ -2,13 +2,30 @@
   (:require [c-loja.db :as l.db]
             [c-loja.logic :as l.logic]))
 
-(defn resumo-por-usuario-ordenado
-  [pedidos]
-  (->> pedidos
-       l.logic/resumo-por-usuario
-       (sort-by :preco-total)
-       reverse))
+(defn gastou-bastante?
+  [info-do-usuario]
+  (> (:preco-total info-do-usuario) 500))
 
 (let [pedidos (l.db/todos-os-pedidos)
-      resumo (resumo-por-usuario-ordenado pedidos)]
-  (println (some #(> (:preco-total %) 500) resumo)))
+      resumo (l.logic/resumo-por-usuario pedidos)]
+  (println (keep gastou-bastante? resumo)))
+
+(defn filter-one
+  [x]
+  (println "filter-one" x)
+  x)
+
+(defn filter-two
+  [x]
+  (println "filter-two" x)
+  x)
+
+(->> (range 50)
+     (mapv filter-one)
+     (map filter-two)
+     println)
+
+(->> '(1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+     (map filter-one)
+     (map filter-two)
+     println)
